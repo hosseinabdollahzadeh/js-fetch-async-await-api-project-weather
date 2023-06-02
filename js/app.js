84c83b4c69111e6b5d071da8091410872199c4fa
hossein <abdollahzadeh.hossein@gmail.com>
2023-06-02 19:01:29 +0330

    change location

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 const weather = new Weather('tehran', 'tehran');
-// weather.changeLocation('shiraz', 'fars');
 
 const ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
+document.getElementById('w-change-btn').addEventListener('click', changeLocation);
+
+function changeLocation() {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    const countryCode = document.getElementById('countryCode').value;
+
+    weather.changeLocation(city, state, countryCode);
+
+    getWeather();
+
+    $('#locationModal').modal('hide');
+}
 
 function getWeather() {
     weather.getWeather()
